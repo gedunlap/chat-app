fcbec01f0aeb2ab995bc0f9a51d0e4c45e32b94c
gedunlap <g.e.dunlap90@gmail.com>
2021-09-14 11:06:34 -0400

    chats away

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine'
+
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="0e592cd0-47de-4fbb-95ca-b4a79cad2e62"
+            userName="GarrettIsHllywd"
+            userSecret="123123"
+        />
+    )
+}
+
+export default App
