6ceaed36b316c0e68f6a8986534a8ad22e16b267
gedunlap <g.e.dunlap90@gmail.com>
2021-09-22 11:45:16 -0400

    THE P TAG UGH

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine'
 
+import ChatFeed from './Components/ChatFeed'
+
 import './App.css'
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
             projectID="0e592cd0-47de-4fbb-95ca-b4a79cad2e62"
             userName="GarrettIsHllywd"
             userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
 }
